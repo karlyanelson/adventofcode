90d3e9295c1f0fd30dee150589d3c10a2fb71c4a
Karly Nelson <karlyanelson@users.noreply.github.com>
2021-12-06 22:20:16 -0600

    add ui

--- src/App.js
+++ src/App.js
@@ -1,3 +1,74 @@
-export function App() {
-  return <h1>Hello world!</h1>;
+import "./styles.css";
+import { useState } from "react";
+import day1 from "./2021/days/day1";
+import day2 from "./2021/days/day2";
+import day3 from "./2021/days/day3";
+import day4 from "./2021/days/day4";
+import DayButton from "./DayButton";
+
+export default function App() {
+  const [answer, setAnswer] = useState(["no answer", "no answer"]);
+  const [selectedDay, setSelectedDay] = useState(null);
+
+  const isEven = selectedDay % 2 === 0;
+
+  const answers = [day1, day2, day3, day4];
+
+  const noAnswers = new Array(25 - answers.length).fill(0);
+
+  let emptyDays = [];
+
+  noAnswers.forEach((day, index) => {
+    emptyDays.push(answers.length + index + 1);
+  });
+
+  return (
+    <div className="App">
+      <h1>Advent of Code 2021</h1>
+
+      <div aria-live="polite" aria-atomic="true">
+        {selectedDay && (
+          <div className="answers">
+            <h2>
+              Answers for{" "}
+              <span
+                className="answers-day"
+                style={{ background: isEven ? "green" : "red" }}
+              >
+                Day {selectedDay}
+              </span>
+            </h2>
+            <div>
+              {answer.map((ans, index) => (
+                <p key={`answers_${index}`}>
+                  Part&nbsp;{index + 1}:<code>{ans || "TBD"}</code>
+                </p>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className="grid">
+        {answers.map((day, index) => (
+          <div className="grid-item">
+            <DayButton
+              key={`dayButton_${index}`}
+              day={index + 1}
+              onClick={() => {
+                setAnswer(day);
+                setSelectedDay(index + 1);
+              }}
+            />
+          </div>
+        ))}
+        {emptyDays &&
+          emptyDays.map((day, index) => (
+            <div key={`emptyDays_${index}`} className="grid-item">
+              {day}
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/DayButton.js
@@ -0,0 +1,3 @@
+export default function ({ day, onClick }) {
+  return <button onClick={onClick}>{day}</button>;
+}
