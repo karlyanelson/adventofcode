7c558756ea737f0bbebfdf3577f970851443b934
Karly Nelson <karly.nelson@hey.com>
2023-12-07 10:11:16 -0600

    fix build

--- src/2023/days/day1.test.js
+++ src/2023/days/day1.test.js
@@ -1,37 +1,39 @@
 import { calculate1 } from "./day1";
 
-test("part 1 a", () => {
-  const data = ["1abc2"];
+describe("part 1", () => {
+  it("a", () => {
+    const data = ["1abc2"];
 
-  expect(calculate1(data)).toBe(12);
-});
+    expect(calculate1(data)).toBe(12);
+  });
 
-test("part 1 b", () => {
-  const data = ["pqr3stu8vwx"];
+  it("b", () => {
+    const data = ["pqr3stu8vwx"];
 
-  expect(calculate1(data)).toBe(38);
-});
+    expect(calculate1(data)).toBe(38);
+  });
 
-test("part 1 c", () => {
-  const data = ["a1b2c3d4e5f"];
+  it("c", () => {
+    const data = ["a1b2c3d4e5f"];
 
-  expect(calculate1(data)).toBe(15);
-});
+    expect(calculate1(data)).toBe(15);
+  });
 
-test("part 1 d", () => {
-  const data = ["treb7uchet"];
+  it("d", () => {
+    const data = ["treb7uchet"];
 
-  expect(calculate1(data)).toBe(77);
-});
+    expect(calculate1(data)).toBe(77);
+  });
 
-test("part 1 two numbers in a row", () => {
-  const data = ["treb76uche7t"];
+  it("two numbers in a row", () => {
+    const data = ["treb76uche7t"];
 
-  expect(calculate1(data)).toBe(77);
-});
+    expect(calculate1(data)).toBe(77);
+  });
 
-test("part 1 total", () => {
-  const data = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
+  it("total", () => {
+    const data = ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"];
 
-  expect(calculate1(data)).toBe(142);
+    expect(calculate1(data)).toBe(142);
+  });
 });
