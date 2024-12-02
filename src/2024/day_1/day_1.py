2c7f1c9695d1b95babea409db66d1a956ce76155
Karly Nelson <karly.nelson@hey.com>
2024-12-02 10:12:44 -0600

    day 1 part 1

--- src/2024/day_1/day_1.py
+++ src/2024/day_1/day_1.py
@@ -1,4 +1,58 @@
-def hello_world_day_1_thing():
-    msg = 'hello world day 1'
-    print(msg)
-    return msg
+from aocp import (
+    CustomTransform,
+    SortTransform,
+    MapTransform,
+    ReplaceTransform,
+    IntParser,
+    IntListParser,
+    BoolParser,
+    ChainParser,
+    ListParser,
+    SetParser,
+    TupleParser,
+    DictParser,
+)
+
+def get_day_1_data(input = None):
+    raw_data = ''
+
+    if input:
+        raw_data = input
+    else:
+        with open('input.txt', 'r') as file:
+            raw_data = file.read()  
+
+    parser = ListParser()   
+
+    parsed = parser.parse(raw_data) 
+
+    left = []  
+    right = []
+
+    for item in parsed:
+        split = item.split('   ')
+        left.append((split[0]))
+        right.append((split[1]))  
+
+    left_list = list(map(int, left))
+    right_list = list(map(int, right)) 
+
+    left_sorted = sorted(left_list)
+    right_sorted = sorted(right_list) 
+
+    return left_sorted, right_sorted
+
+def day_1_part_1(left_list:list, right_list:list):
+    diffs = []
+
+    for i in range(len(left_list)):
+        diff = abs(left_list[i] - right_list[i])
+        diffs.append(diff)
+
+    return sum(diffs)
+
+def day_1_part_2(left_list:list, right_list:list):
+    left_list_tranform = list(map(int, left_list))
+    right_list_tranform = list(map(int, right_list))
+
+    #This time, you'll need to figure out exactly how often each number from the left list appears in the right list. Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
--- src/2024/day_1/test_day_1.py
+++ src/2024/day_1/test_day_1.py
@@ -1,4 +1,13 @@
-from day_1 import hello_world_day_1_thing
+from day_1 import get_day_1_data
+from day_1 import day_1_part_1
 
-def test_day_1():
-    assert hello_world_day_1_thing() == 'hello world day 1'
+def test_day_1_part_1():
+    input = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""
+    left, right = get_day_1_data(input)
+
+    assert day_1_part_1(left, right) == 11
