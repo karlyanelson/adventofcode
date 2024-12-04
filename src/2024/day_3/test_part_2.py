782a52b07820ef69dde6ed5d96a0c18bd41f40ca
Karly Nelson <karly.nelson@hey.com>
2024-12-04 16:58:53 -0600

    day 3 part 2 add failing test case

--- src/2024/day_3/test_part_2.py
+++ src/2024/day_3/test_part_2.py
@@ -13,5 +13,11 @@ def test_decode_corrupted_memory_case_2():
     input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)$?don't())+mul(32,64]undo()?mul(8,5))"
     assert decode_corrupted_memory(input) == [[2, 4], [8, 5]]
 
+
+def test_decode_corrupted_memory_case_3():
+    input = """^don't()*mul(884,335)]@@why()$do()~#mul(786,141)
+where()}/what(611,646)!mul(470,537)}"""
+    assert decode_corrupted_memory(input) == [[786,141], [470,537]]
+
 def test_part_2():
     assert answer_part_2(data) == 48
