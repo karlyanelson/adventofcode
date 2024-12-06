881f5ca414cb833980dfd06da602ca963697fd81
Karly Nelson <karly.nelson@hey.com>
2024-12-06 15:17:25 -0600

    day 4 wip

--- src/2024/day_4/part_1.py
+++ src/2024/day_4/part_1.py
@@ -1,20 +1,12 @@
-import re
-
-def decode_corrupted_memory(string) -> list[list[int]]:
-    # for a string like xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-    # write regex that will match the numbers between two parentheses
-
-    pattern = re.compile(r'mul(?<!\?)\((\d{1,3}),(\d{1,3})\)')
-    matches = pattern.findall(string)
-    result = [[int(x), int(y)] for x, y in matches]
-    return result
+def count_in_rows(rows:list[str]) -> int:
+    total = 0
+    for row in rows:
+        total += row.count('XMAS')
+        
+    return total
 
 def answer_part_1(string):
-    memory = decode_corrupted_memory(string)
-    total = 0
-
-    for x, y in memory:
-        total += x * y
+    in_rows = count_in_rows(string)
     
-    return total
+    return in_rows
 
--- src/2024/day_4/test_part_1.py
+++ src/2024/day_4/test_part_1.py
@@ -1,5 +1,5 @@
-from day_3.data import get_data
-from day_3.part_1 import answer_part_1, decode_corrupted_memory
+from day_4.data import get_data
+from day_4.part_1 import answer_part_1, count_in_rows
 
 
 input = """MMMSXXMASM
@@ -13,10 +13,16 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX"""
 
-data= get_data(input)
+rows, matrix = get_data(input)
 
 def test_get_data():
-    assert get_data(input) == input
+    test_rows = ['MMMSXXMASM', 'MSAMXMSMSA', 'AMXSXMAAMM', 'MSAMASMSMX', 'XMASAMXAMM', 'XXAMMXXAMA', 'SMSMSASXSS', 'SAXAMASAAA', 'MAMMMXMMMM', 'MXMXAXMASX']
+    # test_matrix = [['M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'], ['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'], ['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'], ['M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'], ['X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'], ['X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'], ['S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'], ['S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'], ['M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'], ['M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X']]
+    
+    assert get_data(input) == test_rows
+
+def test_count_in_rows():
+    assert count_in_rows(rows) == 18
 
 def test_part_1():
-    assert answer_part_1(data) == 18
+    assert answer_part_1(rows, matrix) == 18
